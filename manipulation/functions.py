2ce5ffe2084c4e9d7febf599e2e53caa268d49ae
salvatorepuccio <s.puccio1@studenti.unipi.it>
2022-06-29 18:03:34 +0200

    T

---
+++ manipulation/functions.py
@@ -0,0 +1,201 @@
+from turtle import clear
+from attr import attributes
+import pandas as pd
+import matplotlib.pyplot as plt 
+import matplotlib.dates as mdates
+from enum import Enum
+
+def fill_dates(date_start,date_end):
+    # filling data_format
+    dates = pd.date_range(date_start,date_end)
+    ret = []
+    for x in dates:
+        ret.append(x.date())
+    return ret
+
+def get_day_in_year(l):
+    l1 = []
+    for val in l:
+        l1.append(val.timetuple().tm_yday)
+    return l1
+
+# mette degli 0 nella date in cui non ci sono state vendite
+def fill_dataframe(df,df_columns):
+    full_calendar = fill_dates(str(df['data_format_date'][0]),str(df['data_format_date'][len(df)-1]))
+    j = 0
+    N_date_mancanti = 0
+    for curr_date in full_calendar:
+        
+        if(str(curr_date).split(" ")[0] == str(df['data_format_date'][j])):
+            # Se la data corrente e' una di quelle in cui ho fatto una vendita, vai avanti
+            j = j + 1
+        else:
+            # altrimenti metti una riga che indice che non ho venduto nulla
+            new_row = [curr_date,0,0,-1]#['data_format_date','qta','val','flag_off']
+            dff = pd.DataFrame([new_row],columns=df_columns)
+            df = pd.concat([df,dff])
+            N_date_mancanti += 1
+    
+    df["data_format_date"] = pd.to_datetime(df["data_format_date"])
+    df = df.sort_values(by="data_format_date")
+    print("Mancavano "+str(N_date_mancanti)+" date")
+    df = df.reset_index(drop=True)
+    return df
+
+def fill_dataframe_all_stores(df,df_columns):
+    full_calendar = fill_dates(str(df['data_format_date'][0]),str(df['data_format_date'][len(df)-1]))
+    j = 0
+    N_date_mancanti = 0
+    for curr_date in full_calendar:
+        
+        if(str(curr_date).split(" ")[0] == str(df['data_format_date'][j])):
+            # Se la data corrente e' una di quelle in cui ho fatto una vendita, vai avanti
+            j = j + 1
+        else:
+            # altrimenti metti una riga che indice che non ho venduto nulla
+            new_row = [curr_date,999,0,0,-1]#['data_format_date','rag_soc','qta','val','flag_off']
+            dff = pd.DataFrame([new_row],columns=df_columns)
+            df = pd.concat([df,dff])
+            N_date_mancanti += 1
+    
+    df["data_format_date"] = pd.to_datetime(df["data_format_date"])
+    df = df.sort_values(by="data_format_date")
+    print("Mancavano "+str(N_date_mancanti)+" date")
+    df = df.reset_index(drop=True)
+    return df
+
+def somma_duplicate(df):
+    data_prec = "NULL"
+    i = 0
+    eliminate = 0
+    end = len(df)
+    for i in range(0,end):
+        if(str(df['data_format_date'][i]) == data_prec):
+            eliminate+=1
+            df['qta'][i] += df['qta'][i-1]
+            df['val'][i] += df['val'][i-1]
+            df = df.drop(i-1)
+            end -= 1
+
+        data_prec = str(df['data_format_date'][i])
+    # print("Eliminate "+str(eliminate)+" duplicazioni")
+    df = df.reset_index(drop=True)
+    return df
+        
+def myplot2(x,y,y1,title):
+    fig, ax = plt.subplots()
+    ax = plt.gca() 
+    formatter = mdates.DateFormatter("%Y-%m-%d")
+    ax.xaxis.set_major_formatter(formatter)
+    locator = mdates.WeekdayLocator(interval=4)
+    ax.xaxis.set_major_locator(locator)
+    plt.grid(True)
+    plt.gcf().autofmt_xdate()
+    plt.title(title)
+    ax.plot(x,y,linewidth=1)
+    ax.plot(x[-len(y1):],y1,color='red',linewidth=1)
+    
+    mng = plt.get_current_fig_manager()
+    mng.full_screen_toggle()
+    plt.show()
+
+def myplot(x,y):
+    fig, ax = plt.subplots()
+    ax = plt.gca()
+    formatter = mdates.DateFormatter("%Y-%m-%d")
+    ax.xaxis.set_major_formatter(formatter)
+    locator = mdates.WeekdayLocator(interval=4)
+    ax.xaxis.set_major_locator(locator)
+    plt.grid(True)
+    plt.gcf().autofmt_xdate()
+    ax.plot(x,y,linewidth=1)
+    mng = plt.get_current_fig_manager()
+    mng.full_screen_toggle()
+    plt.show()
+
+def print_all(df):
+    with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+        print(df)
+
+def get_day_in_week(df_col):
+    ret = []
+    for x in df_col:
+        ret.append(x.weekday())
+    return ret
+
+def get_week_in_year(df_col):
+    ret = []
+    for x in df_col:
+        ret.append(x.isocalendar()[1])
+    return ret
+
+def get_avg_price(df_qta_val):
+    sum = 0
+    avg = 0
+    n = 0
+    for i in range(0,len(df_qta_val)):
+        if(df_qta_val['qta'][i]>0):
+            n+=1
+            sum += df_qta_val['val'][i] / df_qta_val['qta'][i]
+    # print("Somma dei prezzi unitari: "+str(sum))
+
+    avg = sum / n
+    # print("Media dei prezzi unitari: "+str(avg))
+    return avg
+
+def get_unit_price(df_qta_val):
+    avg_price = get_avg_price(df_qta_val)
+    ret = []
+    unit_price=0
+    for i in range(0,len(df_qta_val)):
+        if(df_qta_val['qta'][i]==0):
+            unit_price = 0
+        else:
+            unit_price = df_qta_val['val'][i] / df_qta_val['qta'][i]
+        ret.append(unit_price)
+        # print("qta "+str(df_qta_val['qta'][i])+" val "+str(df_qta_val['val'][i])+" prezzo unitario "+str(append_me))
+    return ret
+
+def get_day_in_month(df_date):
+    ret = []
+    for x in df_date:
+        ret.append(int(x.strftime("%d")))
+    return ret
+
+def convert_flag_to_bool(df_flag):
+    ret = []
+    for x in df_flag:
+        if(str(x)== str(0)):
+            ret.append(False)
+        elif(str(x) == str(1)):
+            ret.append(True)
+        else:
+            print("Flag: "+str(x))
+    return ret
+
+def stophere():
+    raise SystemExit(0)  
+
+def scala(X_train):
+    from sklearn import preprocessing
+    scaler = preprocessing.StandardScaler().fit(X_train)
+    return scaler.transform(X_train)
+
+def convert_and_get_rag_soc_to_int(df_rag_soc):
+    ret  = []
+    for x in df_rag_soc:
+        if type(x) == str:
+            # print("Current elment: ",x)
+            elems = x.split(" ")
+            appendme = 0
+            if(elems[0]=='IPERSTORE'):
+                appendme = 100
+            if(elems[0]=='SUPERSTORE'):
+                appendme = 200
+            if(elems[0]=='SUPERMARKET'):
+                appendme = 300
+            appendme+=int(elems[1])
+        # print("Converted: ",appendme)
+        ret.append(appendme)
+    return ret
+        
--- manipulation/mlp_single_store.py
+++ manipulation/mlp_single_store.py
@@ -11,163 +11,16 @@ from sklearn.neural_network import MLPRegressor
 from sklearn.datasets import make_regression
 from sklearn.model_selection import train_test_split 
 import matplotlib.dates as mdates
-from datetime import datetime
-import math
 import time
-from sklearn import metrics
-import seaborn as sns
 import warnings
-from sklearn.exceptions import ConvergenceWarning
+from functions import *
 warnings.filterwarnings("ignore")
 # prende una data di inizio e una data di fine (stringhe) e restituisce una lista di tutti i giorni in mezzo
-def fill_dates(date_start,date_end):
-    # filling data_format
-    dates = pd.date_range(date_start,date_end)
-    ret = []
-    for x in dates:
-        ret.append(x.date())
-    return ret
 
-def get_day_of_the_year(l):
-    l1 = []
-    for val in l:
-        l1.append(val.timetuple().tm_yday)
-    return l1
 
-# mette degli 0 nella date in cui non ci sono state vendite
-def fill_dataframe(df,df_columns):
-    full_calendar = fill_dates(str(df['data_format_date'][0]),str(df['data_format_date'][len(df)-1]))
-    j = 0
-    N_date_mancanti = 0
-    for curr_date in full_calendar:
-        
-        if(str(curr_date).split(" ")[0] == str(df['data_format_date'][j])):
-            # Se la data corrente e' una di quelle in cui ho fatto una vendita, vai avanti
-            j = j + 1
-        else:
-            # altrimenti metti una riga che indice che non ho venduto nulla
-            new_row = [curr_date,0,0,-1]#['data_format_date','qta','val','flag_off']
-            dff = pd.DataFrame([new_row],columns=df_columns)
-            df = pd.concat([df,dff])
-            N_date_mancanti += 1
-    
-    df["data_format_date"] = pd.to_datetime(df["data_format_date"])
-    df = df.sort_values(by="data_format_date")
-    # print("Mancavano "+str(N_date_mancanti)+" date")
-    df = df.reset_index(drop=True)
-    return df
 
-def somma_duplicate(df):
-    data_prec = "NULL"
-    i = 0
-    eliminate = 0
-    end = len(df)
-    for i in range(0,end):
-        if(str(df['data_format_date'][i]) == data_prec):
-            eliminate+=1
-            df['qta'][i] += df['qta'][i-1]
-            df['val'][i] += df['val'][i-1]
-            df = df.drop(i-1)
-            end -= 1
 
-        data_prec = str(df['data_format_date'][i])
-    # print("Eliminate "+str(eliminate)+" duplicazioni")
-    df = df.reset_index(drop=True)
-    return df
-        
-def myplot2(x,y,y1,title):
-    fig, ax = plt.subplots()
-    ax = plt.gca() 
-    formatter = mdates.DateFormatter("%Y-%m-%d")
-    ax.xaxis.set_major_formatter(formatter)
-    locator = mdates.WeekdayLocator(interval=4)
-    ax.xaxis.set_major_locator(locator)
-    plt.grid(True)
-    plt.gcf().autofmt_xdate()
-    plt.title(title)
-    ax.plot(x,y,linewidth=1)
-    ax.plot(x[-len(y1):],y1,color='red',linewidth=1)
-    
-    mng = plt.get_current_fig_manager()
-    mng.full_screen_toggle()
-    plt.show()
 
-def myplot(x,y):
-    fig, ax = plt.subplots()
-    ax = plt.gca()
-    formatter = mdates.DateFormatter("%Y-%m-%d")
-    ax.xaxis.set_major_formatter(formatter)
-    locator = mdates.WeekdayLocator(interval=4)
-    ax.xaxis.set_major_locator(locator)
-    plt.grid(True)
-    plt.gcf().autofmt_xdate()
-    ax.plot(x,y,linewidth=1)
-    mng = plt.get_current_fig_manager()
-    mng.full_screen_toggle()
-    plt.show()
-
-def print_all(df):
-    with pd.option_context('display.max_rows', None, 'display.max_columns', None):
-        print(df)
-
-def get_days(df_col):
-    ret = []
-    for x in df_col:
-        ret.append(x.weekday())
-    return ret
-
-def get_week(df_col):
-    ret = []
-    for x in df_col:
-        ret.append(x.isocalendar()[1])
-    return ret
-
-def get_avg_price(df_qta_val):
-    sum = 0
-    avg = 0
-    n = 0
-    for i in range(0,len(df_qta_val)):
-        if(df_qta_val['qta'][i]>0):
-            n+=1
-            sum += df_qta_val['val'][i] / df_qta_val['qta'][i]
-    # print("Somma dei prezzi unitari: "+str(sum))
-
-    avg = sum / n
-    # print("Media dei prezzi unitari: "+str(avg))
-    return avg
-
-def get_unit_price(df_qta_val):
-    avg_price = get_avg_price(df_qta_val)
-    ret = []
-    unit_price=0
-    for i in range(0,len(df_qta_val)):
-        if(df_qta_val['qta'][i]==0):
-            unit_price = 0
-        else:
-            unit_price = df_qta_val['val'][i] / df_qta_val['qta'][i]
-        ret.append(unit_price)
-        # print("qta "+str(df_qta_val['qta'][i])+" val "+str(df_qta_val['val'][i])+" prezzo unitario "+str(append_me))
-    return ret
-
-def get_day_of_month(df_date):
-    ret = []
-    for x in df_date:
-        ret.append(int(x.strftime("%d")))
-    return ret
-
-def convert_flag_to_bool(df_flag):
-    ret = []
-    for x in df_flag:
-        if(str(x)== str(0)):
-            ret.append(False)
-        elif(str(x) == str(1)):
-            ret.append(True)
-        else:
-            print("Flag: "+str(x))
-    return ret
-
-def stophere():
-    raise SystemExit(0)  
 
 client = Client(
                 host='127.0.0.1',
@@ -197,10 +50,52 @@ client.execute("use chpv")
 #140310420     KINDER CARDS LATTE/CAC. GR.128
 #148500405     PERONI BIRRA CL.33X3
 #  ----------------------------------------------
-iperstores = ['IPERSTORE 01','IPERSTORE 02', 'IPERSTORE 03', 'IPERSTORE 04']
-superstores = ['SUPERSTORE 01','SUPERSTORE 02','SUPERSTORE 03','SUPERSTORE 04','SUPERSTORE 05','SUPERSTORE 06','SUPERSTORE 07','SUPERSTORE 08','SUPERSTORE 09','SUPERSTORE 10','SUPERSTORE 11','SUPERSTORE 12','SUPERSTORE 13','SUPERSTORE 14','SUPERSTORE 15']
-supermarkets = ['SUPERMARKET 01','SUPERMARKET 02','SUPERMARKET 03','SUPERMARKET 04','SUPERMARKET 05','SUPERMARKET 06','SUPERMARKET 07','SUPERMARKET 08','SUPERMARKET 09','SUPERMARKET 10','SUPERMARKET 11','SUPERMARKET 12','SUPERMARKET 13','SUPERMARKET 14']
-categories = [iperstores,superstores,supermarkets]
+
+
+iperstores = [
+    'IPERSTORE 01',
+    'IPERSTORE 02', 
+    'IPERSTORE 03', 
+    'IPERSTORE 04'
+    ]
+superstores = [
+    'SUPERSTORE 01',
+    'SUPERSTORE 02',
+    'SUPERSTORE 03',
+    'SUPERSTORE 04',
+    'SUPERSTORE 05',
+    'SUPERSTORE 06',
+    'SUPERSTORE 07',
+    'SUPERSTORE 08',
+    'SUPERSTORE 09',
+    'SUPERSTORE 10',
+    'SUPERSTORE 11',
+    'SUPERSTORE 12',
+    'SUPERSTORE 13',
+    'SUPERSTORE 14',
+    'SUPERSTORE 15'
+    ]
+supermarkets = [
+    'SUPERMARKET 01',
+    'SUPERMARKET 02',
+    'SUPERMARKET 03',
+    'SUPERMARKET 04',
+    'SUPERMARKET 05',
+    'SUPERMARKET 06',
+    'SUPERMARKET 07',
+    'SUPERMARKET 08',
+    'SUPERMARKET 09',
+    'SUPERMARKET 10',
+    'SUPERMARKET 11',
+    'SUPERMARKET 12',
+    'SUPERMARKET 13',
+    'SUPERMARKET 14'
+    ]
+categories = [
+    iperstores,
+    superstores,
+    supermarkets
+    ]
 prodotto = "PERONI BIRRA CL.33X3"
 t = time.time()
 for category in categories:
@@ -224,6 +119,7 @@ for category in categories:
             # Somma le righe duplicate, ovvero con la stessa data
             df = somma_duplicate(df)
             righe_non_nulle = len(df)
+           
             
             # Aggiunge una riga per ogni data mancante all'interno del range della query, con qta = 0
             df = fill_dataframe(df,cols)
@@ -233,26 +129,30 @@ for category in categories:
             # myplot(df['data_format_date'],df['qta'])
             
             # Estrazione nuove info dai dati esistenti
-            day_of_week = get_days(df['data_format_date'])# num [0,6] lun,mar,mer....dom
-            week_n = get_week(df['data_format_date'])# num [0,52] settimana dell'anno
+            day_of_week = get_day_in_week(df['data_format_date'])# num [0,6] lun,mar,mer....dom
+            week_n = get_week_in_year(df['data_format_date'])# num [0,52] settimana dell'anno
             unit_price = get_unit_price(df[['qta','val']])# prezzo unitario del prodotto
-            day_of_year = get_day_of_the_year(df['data_format_date'])# num [0,365] num giorno nell'anno
-            day_of_month = get_day_of_month(df['data_format_date'])# num [0,30] num giorno del mese
+            day_of_year = get_day_in_year(df['data_format_date'])# num [0,365] num giorno nell'anno
+            day_of_month = get_day_in_month(df['data_format_date'])# num [0,30] num giorno del mese
             # flag_off_bool = convert_flag_to_bool(df['flag_off'])# flag offerta True, False invece di 1,0
-
+            
             # Inserimento nuove colonne
-            df.insert(0, 'day_of_month', day_of_month, True)
-            df.insert(0, 'day_of_week', day_of_week, True)
-            df.insert(0,'day_of_year', day_of_year, True)
-            df.insert(0, 'week_n', week_n, True)
-            df.insert(0,'unit_price', unit_price, True)
-            # df.insert(0, 'flag_off_bool', flag_off_bool, True)
+            df.insert(len(df.columns), 'day_of_month', day_of_month, True)
+            df.insert(len(df.columns), 'day_of_week', day_of_week, True)
+            df.insert(len(df.columns),'day_of_year', day_of_year, True)
+            df.insert(len(df.columns), 'week_n', week_n, True)
+            df.insert(len(df.columns),'unit_price', unit_price, True)
+           
+            # df.insert(len(df.columns), 'flag_off_bool', flag_off_bool, True)
 
             y = df['qta'].to_numpy()
             X = df[['day_of_month','day_of_week','day_of_year','week_n','unit_price','flag_off']].to_numpy()
             # X = df[['day_of_year','unit_price','flag_off']].to_numpy()
         
-            X_train, X_test, y_train, y_test = train_test_split(X, y,shuffle=False,test_size=0.20)
+            X_train, X_test, y_train, y_test = train_test_split(X, y,shuffle=False,test_size=0.10)
+            print("Train size: ",len(X_train))
+            print("Test size: ",len(X_test))
+            
             # print("Righe valide in input: ",righe_non_nulle)
             # print(str(tuple))
 
@@ -260,26 +160,28 @@ for category in categories:
             solvers = ['lbfgs','sgd', 'adam']
             learning_rates = ['constant', 'invscaling', 'adaptive']
 
-            mlp_gs = MLPRegressor()
+            # Se utilizzo lbfgs ho biosgno di scalare i dati
+            # X_train = scala(X_train)
+
+            mlp_gs = MLPRegressor(early_stopping=True,validation_fraction=0.3)
             parameter_space = {
-                'hidden_layer_sizes': range(10,100,10),
-                # [(10,),(20,),(30,),(40,),(50,),(60,),(70,),(80,),(90,),(100,)],
+                'hidden_layer_sizes': range(10,200,10),
                 'activation': activators,
-                'solver': solvers,
-                'learning_rate': learning_rates,
-                'max_iter' : range(100,300,50),
-                'random_state' : [35] 
+                'solver': ['adam'],
+                'max_iter' : [2000],
+                'random_state' : [35],
+                "alpha" : [0.001]
             }
             from sklearn.model_selection import GridSearchCV
             t = time.time()
-            clf = GridSearchCV(mlp_gs,parameter_space, n_jobs=-1, cv=5,refit=True,scoring='neg_median_absolute_error')
-            clf.fit(X_train, y_train) # X is train samples and y is the corresponding labels
+            clf = GridSearchCV(mlp_gs,parameter_space, cv=5, n_jobs=-1,refit=True,scoring='explained_variance')
+            clf.fit(X_train, y_train)
             y_pred = clf.predict(X_test)
             y_pred = y_pred.clip(min=0)
             score = clf.score(X_test,y_test)
 
             print('\n\n\n\nBest parameters found:\n', clf.best_params_)
-            print("neg_median_absolute_error")
+            print("explained_variance")
             print("Score: ",score)
             input("Tempo %.2f s\n" % (time.time() - t))
 
@@ -351,6 +253,7 @@ for category in categories:
             # y_test = np.trunc(y_test)
             # print(y_test)
             # print(y_pred)
+
             
                         
         
